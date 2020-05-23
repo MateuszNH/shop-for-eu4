be9729ff8cb8923db99072824ce376e8a5530f91
Mateusz <matim98@tlen.pl>
2020-05-23 17:39:26 +0200

    add registration form with saving users to memory

--- src/main/java/com/listy/shopforeu4/repository/UserRepository.java
+++ src/main/java/com/listy/shopforeu4/repository/UserRepository.java
@@ -3,5 +3,5 @@ package com.listy.shopforeu4.repository;
 import com.listy.shopforeu4.model.User;
 
 public interface UserRepository {
-     void save(User user);
+     void save(User user); // findbymail
 }
--- src/main/java/com/listy/shopforeu4/service/RegisterService.java
+++ src/main/java/com/listy/shopforeu4/service/RegisterService.java
@@ -5,6 +5,9 @@ import com.listy.shopforeu4.model.User;
 import com.listy.shopforeu4.repository.UserRepository;
 import org.springframework.stereotype.Service;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 @Service //spring bean declaration
 public class RegisterService {
 private UserRepository userRepository;
@@ -15,13 +18,30 @@ private UserRepository userRepository;
 
     public void addNewUser(UserDTO userDTO){
         validateFieldsNotEmpty(userDTO);
+        validatePasswdLength(userDTO);
+        validateMailRequirements(userDTO);
         User user = new User(userDTO.getMail(),userDTO.getPasswd());
         userRepository.save(user);
     }
     public void validateFieldsNotEmpty(UserDTO userDTO){
-        if(userDTO.getMail().isEmpty() || userDTO.getPasswd().isEmpty()){
+        if(userDTO.getMail().isEmpty() || userDTO.getPasswd().isEmpty())  {
             throw new RegisterServiceException("pola nie mogą być puste"); // czy hasło ma 6 znaków, czy mail ma poprawną składnie ??? regex
         }
 
+    }
+
+    private void validateMailRequirements(UserDTO userDTO) {
+        String regex = "^[a-zA-Z0-9_!#$%&’*+/=?`{|}~^.-]+@[a-zA-Z0-9.-]+$";
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(userDTO.getMail());
+        if(!matcher.matches()){
+            throw new RegisterServiceException("niepoprawny mail");
+        }
+    }
+
+    private void validatePasswdLength(UserDTO userDTO) {
+        if(userDTO.getPasswd().length()<6){
+        throw new RegisterServiceException("zbyt krótkie hasło");
+        }
     }
 }
