51ca6742e5da2e7850c197c1cc122bc74dbd6e24
Mateusz <matim98@tlen.pl>
2020-05-31 14:17:34 +0200

    implement full register

--- src/main/java/com/listy/shopforeu4/model/User.java
+++ src/main/java/com/listy/shopforeu4/model/User.java
@@ -4,10 +4,15 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity // naprawia błądxD
 @Data //lombok uzupełnia standardowe metody
 @NoArgsConstructor // konstruktor ze wszystkimi argumentam // i
 @AllArgsConstructor
 public class User {
+    @Id
     private String login;
     private String password;
     private boolean leader;
